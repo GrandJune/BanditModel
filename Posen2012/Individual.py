9c865b2828ba9a5a3417f5c56ccb42cc274e509d
毕夏 <978260577@qq.com>
2024-08-26 21:21:35 +0800

    initial version

---
+++ Posen2012/Individual.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time     : 8/26/2024 16:20
+# @Author   : Junyi
+# @FileName: Individual.py
+# @Software  : PyCharm
+# Observing PEP 8 coding style
---
+++ Posen2012/Reality.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time     : 8/26/2024 16:21
+# @Author   : Junyi
+# @FileName: Reality.py
+# @Software  : PyCharm
+# Observing PEP 8 coding style
+import numpy as np
+
+class Reality:
+    def __init__(self, N=None, ):
+
+        self.N = N
